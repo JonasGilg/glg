5ed0019d715c0f53a51314b8a39366324afeae16
jonas <jonas.gilg@stud.hs-hannover.de>
2019-12-26 23:54:22 +0100

    :beetle: Fix using glm::distance instead of glm::length.

--- glg/algorithm/nsphere.hpp
+++ glg/algorithm/nsphere.hpp
@@ -36,13 +36,13 @@ namespace glg {
     /// The distance from the n-sphere center to the given position.
     template<int L, typename T>
     T centerDistance(glg::nsphere<L, T> const &ns, glm::vec<L, T> const &pos) {
-        return glm::length(ns.center, pos);
+        return glm::distance(ns.center, pos);
     }
 
     /// The shortest distance between the surface of the n-sphere and the given position.
     template<int L, typename T>
     T surfaceDistance(glg::nsphere<L, T> const &ns, glm::vec<L, T> const &pos) {
-        return glm::length(ns.center, pos) - ns.radius;
+        return glm::distance(ns.center, pos) - ns.radius;
     }
 
     /// The angular L of the n-sphere as seen from the given position.
@@ -54,13 +54,13 @@ namespace glg {
     /// Distance between two n-sphere centers.
     template<int L, typename T>
     T centerDistance(glg::nsphere<L, T> const &ns1, glg::nsphere<L, T> const &ns2) {
-        return glm::length(ns1.center, ns2.center);
+        return glm::distance(ns1.center, ns2.center);
     }
 
     /// Distance between two n-sphere surfaces.
     template<int L, typename T>
     T surfaceDistance(glg::nsphere<L, T> const &ns1, glg::nsphere<L, T> const &ns2) {
-        return glm::length(ns1.center, ns2.center) - ns1.radius - ns2.radius;
+        return glm::distance(ns1.center, ns2.center) - ns1.radius - ns2.radius;
     }
 
     /// True if the given n-spheres intersect.
@@ -72,13 +72,13 @@ namespace glg {
     /// True if the given position is inside the n-sphere.
     template<int L, typename T>
     bool contains(glg::nsphere<L, T> const& ns, glm::vec<L, T> const& pos) {
-        return glm::length(ns.center, pos) < ns.radius;
+        return glm::distance(ns.center, pos) < ns.radius;
     }
 
     /// True if the second n-sphere is completely inside the first n-sphere.
     template<int L, typename T>
     bool contains(glg::nsphere<L, T> const& ns1, glg::nsphere<L, T> const& ns2) {
-        return glm::length(ns1.center, ns2.center) + ns2.radius < ns1.radius;
+        return glm::distance(ns1.center, ns2.center) + ns2.radius < ns1.radius;
     }
 }
 
